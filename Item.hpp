e0ea4fc310f7bc9f5a4e386c44f750f4864288ca
Nonso Iwu <nonsoiwu@udel.edu>
2019-11-29 14:07:39 -0500

    Added header files for Knapsack and Item classes

---
+++ Item.hpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string.h>
+#include <stdlib.h>
+using namespace std;
+
+/**
+ * Item (or Object) for the KnapSack to hold
+ */
+ 
+class Item{
+    string name;    //Optional name to identify an item
+    int weight;     //Associated weights of an item
+    int value;      //Associated values of an item
+
+    public:
+        Item(string n, int w, int v); //Constructor sets name, weight, and value to n, w, v respectively
+};
+
+/**
+ * Acts as a list for classes of 'Item'
+ */
+class ItemNode{
+    Item *i;            //Address to the item this node references
+    ItemNode *next;     //Address to the next item in list
+
+    public:
+        ItemNode();         //Constructor
+        ItemNode(Item &i);  //Constructor; takes the address of an Item class
+};
---
+++ Knapsack.hpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string.h>
+#include <stdlib.h>
+#include "Item.hpp"
+using namespace std;
+
+/**
+ * Knapsack class for the knapsack problem
+ */
+
+class Knapsack{
+    ItemNode items;
+    int capacity;   //Capacity number s
+
+    public:
+        Knapsack(int c); //Constructor. capacity attribute is set to c
+};
+
+
+void::solve(); //1 function that solves the knapsack problem
