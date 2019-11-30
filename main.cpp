e7e84d8af58e2ba73cd1fabe41bbdef3846b3f00
Nonso Iwu <nonsoiwu@udel.edu>
2019-11-30 16:04:55 -0500

    Minor adjustments

--- main.cpp
+++ main.cpp
@@ -12,9 +12,12 @@ using namespace std;
 
 int main(int argc, char const *argv[])
 {
+    int capacity = 4;
     ItemList i = createMock1();
     Knapsack ks = Knapsack();
     //i.show();
-    ks.solve2(&i, 10);
+    ks.solve2(&i, capacity);
+    cout << "\n";
+    ks.solve(&i, capacity);
     return 0;
 }
